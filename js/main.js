87a54fc325d059a26d976b300abc023fd84d1dbb
ChesnokovPavel <ChesnokovPavel@gmail.com>
2017-11-20 20:31:53 +0200

    gotovo

---
+++ js/main.js
@@ -0,0 +1,109 @@
+// var a = document.querySelector("#q")
+// for(var i=4; i<401; i++ ){
+//     console.log(i);
+//     a.innerHTML+=i+' ';
+// }
+
+// var a = document.querySelector("#q")
+// for(var i=654; i>14; i+=3){
+//     a.innerHTML+=i+' ';
+// }
+
+// var a = document.querySelector("#q")
+// for(var i=654; i>-1; i--){
+//     a.innerHTML+=i+' ';
+// }
+
+// var a = document.querySelector("#q")
+// for(var i=1983; i<2018; i++){
+//     a.innerHTML+=i+' ';
+// }
+
+// var a = document.querySelector("#q")
+// for(var i=-4; i<101; i++){
+//     a.innerHTML+=i+' ';
+// }
+
+// var a = [3, 6, 1, 13, 88, 43];
+// console.log(a[0], a[2]);
+
+// var a = ['Hi', "hello", 34, "prima"];
+// console.log(a[0], a[3]);
+
+// var a = ['Hi', "hello", 34, "prima"];
+// console.log(a.length);
+
+// var a = ['Hi', "hello", 34, "prima", 3, 6, 1, 13, 88, 43];
+// a[2]='new element';
+// console.log(a);
+
+// var a = ['Hi', "hello", 34, "prima", 3, 6, 1, 13, 88, 43];
+// a[1]=22;
+// a[3]=44;
+// console.log(a);
+
+// var a = ['Hi', "hello", 34, "prima", 3, 6, 1, 13, 88, 43];
+// a[11]=100;
+// console.log(a);
+
+// var a = ['Hi', "hello", 34, "prima", 3, 6, 1, 13, 88, 43];
+// a[12]=200;
+// console.log(a[11]);
+
+// var a = ['Hi', "hello", 34, "prima", 3, 6, 1, 13, 88, 43];
+// a[12]=200;
+// console.log(a[11]);
+
+// var arr = [31, 24, 35, 47, 12];
+// console.log(arr[3], arr[60]);
+
+// var arr = [31, 24, 35, 47, 12]
+// for(var f = 1; f<8; f+=3){
+// console.log(arr[f])
+// }
+
+// var arr = ['Hi', "hello", 34, "prima", 3, 6, 1, 13, 88, 43];
+// for(var i=1; i<6; i++){
+// console.log(arr[i])
+// }
+
+// document.querySelector('#e').onclick = f1;
+// function f1(){
+// var b = document.querySelector('#w').value;
+// var arr = ['Hi', "hello", 34, "prima", 3, 6, 1, 13, 88, 43]
+// console.log(arr[b])
+// }
+
+// var out=document.querySelector('q');
+// var arr = ['Hi', "hello", 34, "prima", 3, 6, 1, 13, 88, 43]
+// for (var i=0; i<arr.length; i++){
+//     q.innerHTML+=arr[i]+' ';
+// }
+
+// var out=document.querySelector('q');
+// var arr = ['Hi', "hello", 34, "prima", 3, 6, 1, 13, 88, 43]
+// for (var i=arr.length-1; i>-1; i--){
+//     q.innerHTML+=arr[i]+' ';
+// }
+
+// arr = [2, 3, [4,5]];
+// console.log(arr[0], arr[1]);
+// console.log(arr[2]);
+
+// arr = [2, 3, [4,5]];
+// console.log(arr[2][0], arr[2][1]);
+
+// arr = [2, 3, [4,5]];
+// console.log(arr[2].length);
+
+// arr = [ 2, [3,4,5], [6,7,8]  ];
+// console.log(arr[1][1], arr[2][2]);
+
+// arr = [ 2, [3,4,5], [6,7,8]  ];
+// console.log(arr[1], arr[1][0]);
+
+// arr = [ 2, [3,4,5], [6,7,8,9, 11, 12, 13, 14]  ];
+// console.log(arr[2][0],arr[2][1],arr[2][2],arr[2][3],arr[2][4],arr[2][5],arr[2][6],arr[2][7])
+
+arr = [ ['hi', 3, 'beta'], ['foo', 'jam', 'tritto']  ];
+console.log(arr[1][0], arr[0][2])
